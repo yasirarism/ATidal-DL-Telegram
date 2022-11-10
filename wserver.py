887b321f82822c06592b825dc24faab693856016
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-11-10 11:23:05 +0700

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "<h1>See mirror-leech-telegram-bot <a href='https://www.github.com/anasty17/mirror-leech-telegram-bot'>@GitHub</a> By <a href='https://github.com/anasty17'>Anas</a></h1>"
+    return "<h1>See Tidal Bot <a href='https://github.com/yasirarism/ATidal-DL-Telegram'>@GitHub</a> By <a href='https://github.com/yasirarism'>Yasir</a></h1>"
 
 @app.errorhandler(Exception)
 def page_not_found(e):
-    return f"<h1>404: not found! Mostly wrong input. <br><br>Error: {e}</h2>", 404
+    return f"<h1>404: Not found! Mostly wrong input. <br><br>Error: {e}</h2>", 404
 
 if __name__ == "__main__":
     app.run()
